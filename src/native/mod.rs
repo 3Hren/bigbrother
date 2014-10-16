950d45f6ff5c9f23f2a117f8386ba69cd6b473c3
Evgeny Safronov <division494@gmail.com>
2014-10-16 23:23:42 +0400

    [Code Style] Prettifying.

--- src/native/mod.rs
+++ src/native/mod.rs
@@ -1,7 +1,2 @@
-#[cfg(target_os = "linux")]
-#[path = "linux.rs"]
-pub mod backend;
-
-#[cfg(target_os = "macos")]
-#[path = "macos.rs"]
-pub mod backend;
+#[cfg(target_os = "linux")] #[path = "linux.rs"] pub mod backend;
+#[cfg(target_os = "macos")] #[path = "macos.rs"] pub mod backend;
